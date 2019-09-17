5b9995f19b88d6fa8dbb97b03dc6836460e50300
vesteinnbjarna <vesteinn19@ru.is>
2019-09-17 09:31:37 +0000

    Breytti

---
+++ tile_traveller.py
@@ -0,0 +1,7 @@
+# Create a game a game that consist of a 3x3 grid
+# Where the player can only move in a fixed direction
+# based on the tile he's at
+# The player starts at tile 1,1 and wins once he 
+# reaches 3, 1
+
+def move_north (vert, hor):
