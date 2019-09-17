2dcb2a88cf271a16227a457f62b49e441f6ea50d
DonNinja <yngvileo@gmail.com>
2019-09-17 09:52:28 +0000

    settum upp basic

--- tile_traveller.py
+++ tile_traveller.py
@@ -3,5 +3,21 @@
 # based on the tile he's at
 # The player starts at tile 1,1 and wins once he 
 # reaches 3, 1
+location = 1
 
-def move_north (vert, hor):
+def move_north():
+    return .1
+def move_south():
+    return -.1
+def move_west():
+    return -1
+def move_east():
+    return 1
+
+location += move_west()
+
+while 1.1 <= location <= 1.3 or 2.1 <= location <= 2.3 or 3.1 <= location <= 3.3:
+    
+else:
+    print("invalid direction")
+    location = prev_location
