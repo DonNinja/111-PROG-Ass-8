0daa03925cbe1c548bc176ae7e16cc6d83a411f5
vesteinnbjarna <vesteinn19@ru.is>
2019-09-17 11:06:48 +0000

    Kláruðum verkefnið

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,3 +1,4 @@
+# Unnið af Vésteini Bjarnasyni og Yngvi Leó Þráinsson
 # Create a game a game that consist of a 3x3 grid
 # Where the player can only move in a fixed direction
 # based on the tile he's at
@@ -5,6 +6,7 @@
 # reaches 3, 1
 location = 11
 
+# Functions for moving in each direction:
 def move_north():
     return 1
 def move_south():
@@ -14,7 +16,7 @@ def move_west():
 def move_east():
     return 10
 
-
+# a function that changes the position
 def user_direction(user_input, location):
     if user_input.upper() == 'N':
        location += move_north()
@@ -28,30 +30,38 @@ def user_direction(user_input, location):
     elif user_input.upper() == 'W':
         location += move_west()
     return location
-        
+
+# A function that shows valid direction      
 def whereTo(location):
     if location == 11 or location == 21:
-        print("You can travel (N)orth")
+        print("You can travel: (N)orth.")
     elif location == 12:
-        print("You can travel (N)orth or (E)ast or (S)outh")
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
     elif location == 13:
-        print("You can travel (E)ast or (S)outh")
+        print("You can travel: (E)ast or (S)outh.")
     elif location == 23:
-        print("You can travel (E)ast or (W)est")
+        print("You can travel: (E)ast or (W)est.")
     elif location == 22 or location == 33:
-        print("You can travel (S)outh or (W)est")
+        print("You can travel: (S)outh or (W)est.")
     elif location == 32:
-        print("You can travel (N)orth or (S)outh")
+        print("You can travel: (N)orth or (S)outh.")
         
-print(location)
 
+whereTo(location)
+# A while loop that keeps running while the the location of the player is not 31
+# If it reaches 31 that players wins
 while location != 31:
     user_input = input('Direction: ')
     prev_location = location
-    if location >= 10 and location <= 13 or location >= 21 and location <= 23 or location >= 31 and location <= 33:
+    location = user_direction(user_input, location)
+    
+    # 11 means 1,1 and so forth
+    if location >= 11 and location <= 13 or location >= 21 and location <= 23 or location >= 31 and location <= 33:    
         whereTo(location)
-        print(user_direction(user_input, location))
-        location = user_direction(user_input, location)
-    if not(location >= 10 and location <= 13 or location >= 21 and location <= 23 or location >= 31 and location <= 33):
-        print("invalid direction")
+
+    elif not(location >= 11 and location <= 13 or location >= 21 and location <= 23 or location >= 31 and location <= 33):
+        print("Not a valid direction!")
         location = prev_location
+
+else: 
+    print("Victory!")
